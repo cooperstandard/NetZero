c0c9723ee770a07ea861c2cca35636b08897a8ae
Cooper Standard <cooper@cooperstandard.org>
2025-08-27 17:53:27 -0700

    get balances

---
+++ internal/routes/HandleGetBalance.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/cooperstandard/NetZero/internal/database"
+	"github.com/cooperstandard/NetZero/internal/util"
+	"github.com/google/uuid"
+)
+
+func (cfg *APIConfig) HandleGetBalance(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		groupID string `json:"group_id"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	balances, err := cfg.DB.GetBalanceForUserByGroup(r.Context(), database.GetBalanceForUserByGroupParams{
+		GroupID: uuid.NullUUID{Valid: true, UUID: uuid.MustParse(params.groupID)},
+		UserID:  uuid.NullUUID{Valid: true, UUID: r.Context().Value(UserID{}).(uuid.UUID)},
+	})
+
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't locate balance records", err)
+		return
+	}
+
+	util.RespondWithJSON(w, 200, balances)
+}
