a5a39094b25c0e47417a1e87b77e9d0b344a29cf
Cooper Standard <cooper@cooperstandard.org>
2025-08-28 22:33:25 -0700

    prep for numerics

--- internal/routes/handleTransaction.go
+++ internal/routes/handleTransaction.go
@@ -5,9 +5,12 @@ import "net/http"
 func (cfg *APIConfig) HandleCreateTransactions(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
 		Transactions []struct {
-			Debtor   string  `json:"debtor"`
-			Creditor string  `json:"creditor"`
-			Amount   float64 `json:"amount"` //TODO: not sure if this is how amount should be modelled.
+			Debtor   string `json:"debtor"`
+			Creditor string `json:"creditor"`
+			Amount   struct {
+				Dollars int `json:"dollars"`
+				Cents   int `json:"cents"`
+			} `json:"amount"` //TODO: not sure if this is how amount should be modelled.
 		} `json:"transactions"`
 		GroupID string `json:"group_id"`
 	}
--- internal/util/default.go
+++ internal/util/default.go
@@ -5,3 +5,15 @@ import "fmt"
 func FormPath(method, route, basePath string) string {
 	return fmt.Sprintf("%s %s%s", method, basePath, route)
 }
+
+func ValidateAndFormNumericString(dollars, cents int) (string, error) {
+	if cents < 0 || cents > 99 {
+		return "", fmt.Errorf("%d is not a valid value for cents, cents should be positive and less than 100", cents)
+	}
+	if dollars < 0 {
+
+		return "", fmt.Errorf("%d is not a valid value, dollars should be positive", dollars)
+	}
+
+	return fmt.Sprintf("%20d.%d", dollars, cents), nil
+}
