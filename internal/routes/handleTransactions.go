c005f7e88336b8d6faba9a2a1f96a9ca8efafd96
Cooper Standard <cooper@cooperstandard.org>
2025-09-20 22:24:37 -0700

    updates to transaction handlers

--- internal/routes/handleTransactions.go
+++ internal/routes/handleTransactions.go
@@ -34,11 +34,10 @@ func (cfg *APIConfig) HandleCreateTransactions(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	// TODO: this, need a way to add new debts to an existing transaction and limit the number of transactions which may be added in any one call to prevent locking up the database and web server
-	// if len(params.Transactions) > 50 || len(params.Transactions) == 0 {
-	// 	util.RespondWithError(w, 400, "please batch transactions into groups of 50 or fewer to prevent service slow downs", fmt.Errorf("invalid number of transactions: %d", len(params.Transactions)))
-	// 	return
-	// }
+	if len(params.Transactions) > 50 || len(params.Transactions) == 0 {
+		util.RespondWithError(w, 400, "please batch records into groups of 50 or fewer to prevent service slow downs", fmt.Errorf("invalid number of records: %d", len(params.Transactions)))
+		return
+	}
 
 	transaction, err := cfg.DB.CreateTransaction(r.Context(), database.CreateTransactionParams{
 		Title:       params.Title,
@@ -91,4 +90,63 @@ func recordDebt(cfg APIConfig, ctx context.Context, debt database.CreateDebtPara
 	if err != nil {
 		failedChan <- debt
 	}
+}
+
+func (cfg *APIConfig) HandleAddDebtToTransaction(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		TransactionID string `json:"transaction_id"`
+		Transactions  []struct {
+			Debtor string `json:"debtor"`
+			Amount struct {
+				Dollars int `json:"dollars"`
+				Cents   int `json:"cents"`
+			} `json:"amount"`
+		} `json:"transactions"`
+		Creditor string `json:"creditor"`
+		GroupID  string `json:"group_id"`
+		Title    string `json:"title"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		util.RespondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
+		return
+	}
+
+	okChan := make(chan bool)
+	failedChan := make(chan database.CreateDebtParams)
+	var failed []database.CreateDebtParams
+
+	for _, v := range params.Transactions {
+		go recordDebt(*cfg, r.Context(), database.CreateDebtParams{
+			Amount:        fmt.Sprintf("%d.%d", v.Amount.Dollars, v.Amount.Cents), // TODO: validate the Amount
+			TransactionID: uuid.MustParse(params.TransactionID),
+			Debtor:        uuid.MustParse(v.Debtor),
+			Creditor:      uuid.MustParse(params.Creditor),
+		}, okChan, failedChan)
+	}
+
+	for range len(params.Transactions) {
+		select {
+		case <-okChan:
+			continue
+		case fail := <-failedChan:
+			failed = append(failed, fail)
+			continue
+		}
+	}
+
+	if len(failed) > 0 {
+		util.RespondWithJSON(w, 206, struct {
+			FailedTransactions []database.CreateDebtParams `json:"failed_transactions"`
+			TransactionID      uuid.UUID                   `json:"transaction_id"`
+		}{failed, uuid.MustParse(params.TransactionID)})
+		return
+	}
+
+	util.RespondWithJSON(w, 200, struct {
+		TransactionID uuid.UUID `json:"transaction_id"`
+	}{uuid.MustParse(params.TransactionID)})
 }
