a866ed96f8debb535497a98113ae62904e25ba9a
Cooper Standard <cooper@cooperstandard.org>
2025-11-17 18:01:32 -0800

    make another user and join the group again

--- cmd/test/test.go
+++ cmd/test/test.go
@@ -29,7 +29,6 @@ type loginParameters struct {
 // main is the entry point of the functional test suite
 func main() {
 	/*TODO: run some golden test cases here
-	  		05) create another user and have it join the group
 	  		06) create another group, join with user 1, make sure user 2 does not show up as a member
 	    	06) create a debt for user 2
 	  		07) check transaction record for user 1 and user 2
@@ -62,22 +61,33 @@ func main() {
 	// 01) reset db
 	log.Info("reset DB", "successful", reset(client, adminKey))
 
-	// 02) register user
-	user := createAndLoginUser(client, "test@email.com", "password", "testy mctestface")
+	// 02) register user1
+	user1 := createAndLoginUser(client, "test@email.com", "password", "testy mctestface")
 
 	// 03) create group
-	group, err := createGroup(client, "group 1", user.Token)
+	group, err := createGroup(client, "group 1", user1.Token)
 	if err != nil {
 		log.Fatal("group creation failed", "error", err)
 	}
 	log.Info("created group", "group ID", group.ID)
 
 	// 04) join group
-	err = joinGroup(client, "group 1", user.Token)
+	err = joinGroup(client, "group 1", user1.Token)
 
 	if err != nil {
 		log.Fatal("unable to join group")
 	}
 	log.Info("successfully joined the group")
+
+	// 05) create another user and have it join the group
+	user2 := createAndLoginUser(client, "test2@email.com", "password", "another clever name")
+
+	err = joinGroup(client, "group 1", user2.Token)
+	if err != nil {
+		log.Fatal("unable to join group with user 2")
+	}
+
+	log.Info("successfully created second user and joined the group")
+
 }
 
